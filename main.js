cbd3f23ac7e8132068c09d598306f04dce3e1677
Samson1340 <samsonsimeon2004@gmail.com>
2023-01-30 15:49:46 +0300

    Final Commit

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ const listItems = document.querySelector(".list-items");
 const errorMsg = document.querySelector(".error-msg");
 
 
+// On submit validation part 
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let value = newItem.value;
+    newItem.value = "";
 
     if(!checkValue(value)){
 
@@ -16,38 +19,45 @@ form.addEventListener("submit", (e) => {
         newItem.classList.add("error1");
 
     }
-    // else if(!isLetter(value)){
-    //     errorMsg.innerHTML = "The new item not be Number";
-    //     errorMsg.classList.add("error");
-    //     newItem.classList.add("error1");
-
-    // }
-
+    else if(!isLetter(value)){
+        errorMsg.innerHTML = "The new item not be Number";
+        errorMsg.classList.add("error");
+        newItem.classList.add("error1");
+    }
     else{
         errorMsg.innerHTML = " ";
         errorMsg.classList.remove("error");
         newItem.classList.remove("error1");
         newItem.classList.add("success");
-        creatList(value);
+        createList(value);
     }
-
 });
+
+
+
+// function for to check the empty values 
 
 function checkValue(value) {
     return value.length === 0 ? false : true;
 }
 
-// function isLetter(value) {
-//     return isNumber(value) ? false : true;
-// }
+// function for to check the input is Number or not 
+
+function isLetter(value) {
+    return isNaN(value) ? true : false;
+}
 
 
-function creatList(value){
+
+// function for creating the new to do list items 
+
+function createList(value){
     let div = document.createElement('div');
     div.className = "list-single-item";
 
     let p = document.createElement('p');
     let text = document.createTextNode(value);
+    p.className = "list";
     p.appendChild(text);
 
     let span = document.createElement('span');
@@ -59,13 +69,35 @@ function creatList(value){
     div.appendChild(p);
     div.appendChild(span);
     listItems.appendChild(div);
+    deleteList();
+    completed(value);
+
 }
 
-var close = document.getElementsByClassName("close");
+// Function for completed tasks mark
 
-close.addEventListener("click", function() {
-    let div = this.parentElement;
-    // div.remove();
-    div.style.display = "none";
-});
+function completed(value){
+    const div = document.querySelectorAll(".list-single-item");
+    const list = document.querySelectorAll(".list");
+    for (let i = 0; i < div.length; i++) {
+    div[i].addEventListener("dblclick", (event) => {
+        list[i].textContent = value + " : Completed";
+        list[i].style.color = "green";
+    });
+}
+}
+
+// Function for delete a list function 
+
+function deleteList(){
+const close = document.querySelectorAll(".close");
+for (let i = 0; i < close.length; i++) {
+close[i].addEventListener("click", (event) => {
+        close[i].parentElement.remove();
+    });
+}
+}
+
+
+
 
